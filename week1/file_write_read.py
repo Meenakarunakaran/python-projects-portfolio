fa6fa8aa79a98660a762b3f65e826de66bc36214
Meenakarunakaran <meenakannan18403@gmail.com>
2025-08-21 14:30:28 +0530

    Added week2 bank account project

--- week1/file_write_read.py
+++ week1/file_write_read.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 with open("tasks.txt", "w") as f:
     f.write("Today's task: Complete Python exercises.\n")
 with open("tasks.txt", "a") as f:
@@ -6,6 +5,3 @@ with open("tasks.txt", "a") as f:
 with open("tasks.txt", "r") as f:
     content = f.read()
     print("File Content:\n", content)
-=======
-
->>>>>>> f16940d8546f0059258f6e5ad687ac12717137a3
